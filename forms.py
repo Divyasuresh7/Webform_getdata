b177cf952447ae664f8ff57fc91af25dbfcce02c
Divyasuresh7 <93040292+Divyasuresh7@users.noreply.github.com>
2023-07-06 15:35:25 +0530

    deleted forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField
-
-class SignInForm(FlaskForm):
-    username = StringField('USERNAME')
-    password = PasswordField('PASSWORD')
-    submit = SubmitField('SUBMIT')
-
-
